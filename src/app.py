a615bb46149dc3175568350dfcc3325435216571
fredrikkri <fjkri@hotmail.com>
2023-10-11 12:43:37 +0200

    kan navigere fra login side til hjem
    god endring

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from frontend.routes import user_views
 
 application = Flask(__name__)
 
-user_views.index()
+user_views.display_index()
 
 
 if __name__ == '__main__':
--- src/frontend/routes/user_views.py
+++ src/frontend/routes/user_views.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, request
 
 
 @application.route('/', methods=['GET', 'POST'])
-def index():
+def display_index():
     # if request.method == 'GET':
 
     # connection = sqlite3.connect('..backend/database/database.db')
     # cursor = connection.cursor()
 
     return render_template('index.html')
+
+
+@application.route('/homepage', methods=['GET', 'POST'])
+def display_homepage():
+    # if request.method == 'GET':
+
+    # connection = sqlite3.connect('..backend/database/database.db')
+    # cursor = connection.cursor()
+
+    return render_template('homepage.html')
