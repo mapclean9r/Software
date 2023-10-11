46ec5cfaab50be9ef991d1419eca85dc26886ec3
fredrikkri <fjkri@hotmail.com>
2023-10-11 12:06:02 +0200

    fikk til å vise index
    fra main filen

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 application = Flask(__name__)
 
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
-    if request.methods == 'POST':
+    # if request.method == 'GET':
 
-        connection = sqlite3.connect('..backend/DATABASE.db')
-        cursor = connection.cursor()
+    # connection = sqlite3.connect('..backend/database/database.db')
+    # cursor = connection.cursor()
 
-    return render_template('templates/index.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    application.run(debug=False)
