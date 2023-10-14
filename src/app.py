7293bdb2b41b32b85efcbd8bffd7e1cc6257747c
fredrikkri <fjkri@hotmail.com>
2023-10-14 08:38:36 +0200

    fått laget innputfelt for å lage en tur
    det blir lagt inn i databasen også

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
+import sqlite3
 
 # definerer hvor templates ligger
 application = Flask(__name__, template_folder='frontend/templates')
 
 
 # våre paths:
+
+
 @application.route('/')
 def index():
 
@@ -19,8 +22,37 @@ def registrer_page():
 
 @application.route('/homepage')
 def homepage():
+    db = sqlite3.connect('backend/database/database.db')
+    cursor = db.cursor()
+    cursor.execute("SELECT * from Tour")
+    list_med_turer = cursor.fetchall()
+    db.close()
 
-    return render_template('/homepage.html')
+    return render_template('/homepage.html', turer=list_med_turer)
+
+
+@application.route('/create_a_tour', methods=['POST'])
+def create_a_tour():
+    if request.method == 'POST':
+        title = request.form['Title']
+        description = request.form['Description']
+        country = request.form['Country']
+        location = request.form['Location']
+        date = request.form['Date']
+
+        database = sqlite3.connect('backend/database/database.db')
+        cursor = database.cursor()
+
+        cursor.execute('INSERT INTO Tour (Title, Description, Country, Location, Date) VALUES (?, ?, ?, ?, ?)', (
+            title, description, country, location, date))
+        database.commit()
+        database.close()
+    return redirect(url_for('homepage'))
+
+
+@application.route('/delete_a_tour', methods=['POST'])
+def delete_a_tour():
+    return redirect(url_for('homepage'))
 
 
 if __name__ == '__main__':
