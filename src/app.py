a2f7e3d6ad0fae72bc69ba923f78dd76bef4f735
fredrikkri <fjkri@hotmail.com>
2023-10-16 12:16:30 +0200

    eee
    ee

--- src/app.py
+++ src/app.py
@@ -44,6 +44,9 @@ def registrer_page():
 
         print(username, password, is_admin)
 
+        new_login = UserRegister(username, password, is_admin)
+        UserRegister.register_user_in_database(new_login)
+
     return render_template('/registrer.html')
 
 
