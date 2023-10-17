c6d50f63480e8be65fce1d824204622184a359a8
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-10-17 16:27:42 +0200

    working on search bar in tours

--- src/backend/database/Tour.py
+++ src/backend/database/Tour.py
@@ -4,19 +4,9 @@ pathing = os.path.dirname(__file__) + "/database.db"
 con = sqlite3.connect(pathing)
 cur = con.cursor()
 
+
 def Tour_create(Title, Description, Country, Location, Date):
-    
-
     con.execute("INSERT INTO Tour(Title,Description,Country,Location,Date) VALUES(?,?,?,?,?)",
-                (Title, Description, Country, Location, Date))
-    con.commit()
-    con.close()
-
-
-
-
-def Tour_create_values_into_table(Title,Description,Country,Location,Date):
-    con.execute("INSERT INTO Tour(Title,Description,Country,Location,Date) VALUES('Tour','Nice tour','Tourland','Tour Ave','2023-01-01')",
                 (Title, Description, Country, Location, Date))
     con.commit()
     con.close()
