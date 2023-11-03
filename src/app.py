d3abdd8de826411374524e91698900ab92aead16
HomandoBruther <76850754+HomandoBruther@users.noreply.github.com>
2023-11-03 13:25:58 +0100

    added users page. need to get integrated into adminpage and loose coupled

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask, render_template, url_for, redirect, request
 
 from backend.database.Tour import *
@@ -111,6 +113,8 @@ def checkbox_tour():
         elif action == 'admin':
             if get_user_online_is_admin():
                 return redirect(url_for('adminpage'))
+        elif action == 'users':
+            return redirect(url_for('users'))
         database.commit()
         database.close()
     return redirect(url_for('homepage'))
@@ -168,9 +172,31 @@ def adminpage():
     WHERE TourBooked.User_ID = ?''', (global_user_id,))
 
     list_of_bought_tours = cursor.fetchall()
+
+    #list_of_users = get_user_list()
+
     db.close()
 
+
     return render_template('/adminpage.html', list_of_tours=list, list_of_bought_tours=list_of_bought_tours)
+
+
+def get_user_list():
+    db = sqlite3.connect('backend/database/database.db')
+    cursor = db.cursor()
+    cursor.execute("SELECT Username FROM User")
+    users = cursor.fetchall()
+    db.close()
+
+    return [user[0] for user in users]
+
+
+@application.route('/users')
+def users():
+    list_of_users = get_user_list()
+
+    return render_template('/users.html', users=list_of_users)
+
 
 
 if __name__ == '__main__':
