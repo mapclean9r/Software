56f30f566002740e4739768d79c2e1f8d83d2c34
fredrikkri <fjkri@hotmail.com>
2023-11-08 15:25:52 +0100

    laget en test

--- test/test_tour.py
+++ test/test_tour.py
@@ -2,11 +2,6 @@ from src.backend.database.Tour import Tour_create, Tour_get_all, Tour_find_title
     Tour_delete_if_title_is_provided
 
 
-def test_a_new_tour_is_not_none():
-    new_tour_id = Tour_create("skitur", "kul tur", "Norge", "Finmark", "2023-10-12", "krøsstvællæ")
-    assert new_tour_id is not None
-    Tour_delete_if_title_is_provided("skitur")
-
 def test_a_tours_id_is_of_int_datatype():
     new_tour_id = Tour_create("Fotball på løkka med guttæne", "rått", "gg", "Kautokeino", "2028-04-01", "GrusBane Hansen")
     assert isinstance(new_tour_id, int)
