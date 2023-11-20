1bce2cd6a069b6905f3ef0ec387364ba291a177a
fredrikkri <fjkri@hotmail.com>
2023-11-20 18:03:31 +0100

    Update test_tour_delete.py

--- test/test_tour_delete.py
+++ test/test_tour_delete.py
@@ -17,9 +17,9 @@ def test_a_user_can_delete_a_tour_that_he_has_created():
 
     n_of_created_tours2 = len(tours_that_i_have_created(user_id))
 
+    assert n_of_created_tours != n_of_created_tours2
     assert n_of_created_tours - 1 == n_of_created_tours2
     assert n_of_created_tours - 2 != n_of_created_tours2
     assert n_of_created_tours + 5 != n_of_created_tours2
-
 
     #remove_user_from_list(user_id, "delete")
